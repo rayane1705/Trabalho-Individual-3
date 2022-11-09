1980e353885de53bbbe9dc50e8c93a883c927f04
Rayane <rayanemoraes1705@hotmail.com>
2022-11-09 15:40:09 -0300

    AtualizarNode

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
-const port = process.env.PORT || 3000;
 
 server.use(middlewares)
 server.use(router)
+
+const port = process.env.PORT || 3000;
+
 server.listen(port, () => {
-  console.log('JSON Server is running on: ', port)
+    console.log(`JSON Server is running on: ${port}`)
 })
